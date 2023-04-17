660be33d023263055beb141597ed453565770178
Alex Roedel <roedela@students.westerntc.edu>
2023-04-17 15:02:17 -0500

    Final project code

--- gpio.py
+++ gpio.py
@@ -9,7 +9,7 @@ import wavio as wv
 pygame.init() #initialize pygame
 
 #set each sound as a variable that can be referenced
-octive3 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C3.wav",
+octave3 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C3.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Db3.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/D3.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Eb3.wav",
@@ -22,7 +22,7 @@ octive3 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C3.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Bb3.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/B3.wav"]
 
-octive4 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C4.wav",
+octave4 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C4.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Db4.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/D4.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Eb4.wav",
@@ -35,7 +35,7 @@ octive4 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C4.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Bb4.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/B4.wav"]
 
-octive5 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C5.wav",
+octave5 = ["/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/C5.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Db5.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/D5.wav",
            "/home/alex/Desktop/github/capstone/gpio-music-box/piano-mp3/Eb5.wav",
@@ -62,9 +62,9 @@ custom = ["/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom
           "/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom12.wav"]
 
 btnPressed = [True, True, True, True, True, True, True, True, True, True, True, True]
-pin=[12,20,16,26,19,13,6,12,0,7,8,11]
+pin=[21,20,16,26,19,13,6,12,0,7,8,11]
 GPIO.setmode(GPIO.BCM)
-for i in range(0, 12)
+for i in range(0, 12):
     GPIO.setup(pin[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 swch_pos1 = (9)
@@ -72,6 +72,9 @@ swch_pos3 = (10)
 
 btn_mode = (23)
 btn_rec = (24)
+
+mode_LED = (18) 
+rec_LED = (22)
 
 freq = 44100
 duration = 5
@@ -84,6 +87,9 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
 def record(fileNo):
     print("Now recording...")
     recording = sd.rec(int(duration*freq), samplerate = freq, channels=2)
@@ -92,35 +98,8 @@ def record(fileNo):
     wv.write("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom%d.wav" %fileNo, recording, freq, sampwidth = 2)
     #os.system("arecord /home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom%d.wav -D plughw:CARD=1 -f cd -d 5 &" %fileNo)
 
-def clearCustom():
-    custom1 = None
-    custom2 = None
-    custom3 = None
-    custom4 = None
-    custom5 = None
-    custom6 = None
-    custom7 = None
-    custom8 = None
-    custom9 = None
-    custom10 = None
-    custom11 = None
-    custom12 = None
-    update = True
-
-def setupCustom():
-    custom1 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom1.wav")
-    custom2 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom2.wav")
-    custom3 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom3.wav")
-    custom4 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom4.wav")
-    custom5 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom5.wav")
-    custom6 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom6.wav")
-    custom7 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom7.wav")
-    custom8 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom8.wav")
-    custom9 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom9.wav")
-    custom10 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom10.wav")
-    custom11 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom11.wav")
-    custom12 = pygame.mixer.Sound("/home/alex/Desktop/github/capstone/gpio-music-box/CustomSounds/custom12.wav")
-    update = False
+GPIO.output(18,0)
+GPIO.output(22,0)
 
 print("switch pos1: " + str(GPIO.input(swch_pos1)))
 print("switch pos3: " + str(GPIO.input(swch_pos3)))
@@ -128,31 +107,39 @@ print("Mode Btn: " + str(GPIO.input(btn_mode)))
 print("Record Btn: " + str(GPIO.input(btn_rec)))
 
 while 1:
-    if GPIO.input(swch_pos1) == 0 and GPIO.input(swch_pos3) == 1 and GPIO.input(btn_mode) == 1 and GPIO.input(btn_rec) == 1: #Check that position switch is in position 1
+    if GPIO.input(swch_pos1) == 0 and GPIO.input(swch_pos3) == 1 and GPIO.input(btn_mode) == 1 and GPIO.input(btn_rec) == 1:#Check that position switch is in position 1
+        GPIO.output(18,0)
+        GPIO.output(22,0)
         print("switch pos1: " + str(GPIO.input(swch_pos1)))
         print("switch pos3: " + str(GPIO.input(swch_pos3)))
         time.sleep(0.15)
         for i in range(0,12):
             btnPressed[i] = GPIO.input(pin[i])
             if btnPressed[i] == False:
-                pygame.mixer.Sound(octave3[i]).play                
+                pygame.mixer.Sound(octave3[i]).play()               
     elif GPIO.input(swch_pos1) == 1 and GPIO.input(swch_pos3) == 1 and GPIO.input(btn_mode) == 1 and GPIO.input(btn_rec) == 1: #Check that position switch is in position 2
+        GPIO.output(18,0)
+        GPIO.output(22,0)
         print("switch pos1: " + str(GPIO.input(swch_pos1)))
         print("switch pos3: " + str(GPIO.input(swch_pos3)))
         time.sleep(0.15)
         for i in range(0,12):
             btnPressed[i] = GPIO.input(pin[i])
             if btnPressed[i] == False:
-                pygame.mixer.Sound(octave4[i]).play 
+                sound =pygame.mixer.Sound(octave4[i]).play() 
     elif GPIO.input(swch_pos1) == 1 and GPIO.input(swch_pos3) == 0 and GPIO.input(btn_mode) == 1 and GPIO.input(btn_rec) == 1: #Check that position switch is in position 3
+        GPIO.output(18,0)
+        GPIO.output(22,0)
         print("switch pos1: " + str(GPIO.input(swch_pos1)))
         print("switch pos3: " + str(GPIO.input(swch_pos3)))
         time.sleep(0.15)
         for i in range(0,12):
             btnPressed[i] = GPIO.input(pin[i])
             if btnPressed[i] == False:
-                pygame.mixer.Sound(octave5[i]).play         
+                pygame.mixer.Sound(octave5[i]).play()       
     if GPIO.input(btn_mode) == 0 and GPIO.input(btn_rec) == 0:
+        GPIO.output(18,1)
+        GPIO.output(22,1)
         print("Mode Btn: " + str(GPIO.input(btn_mode)))
         print("Record Btn: " + str(GPIO.input(btn_rec)))
         time.sleep(0.15)
@@ -161,13 +148,12 @@ while 1:
             if btnPressed[i] == False:
                 record(i+1) #filenames are index+1            
     elif GPIO.input(btn_mode) == 0 and GPIO.input(btn_rec) == 1:
-        if update:
-            setupCustom()
+        GPIO.output(18,1)
+        GPIO.output(22,0)
         print("Mode Btn: " + str(GPIO.input(btn_mode)))
         print("Record Btn: " + str(GPIO.input(btn_rec)))
         time.sleep(0.15)
         for i in range(0,12):
             btnPressed[i] = GPIO.input(pin[i])
             if btnPressed[i] == False:
-                pygame.mixer.Sound(custom[i]).play 
-        
+                pygame.mixer.Sound(custom[i]).play()
